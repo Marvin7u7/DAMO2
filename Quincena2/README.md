fa13611c6d8d90929e891363ee4a49f228ea8da2
Marvin7u7 <mg082337@gmail.com>
2022-03-05 18:55:29 -0600

    Implementation websockets

---
+++ Quincena2/README.md
@@ -0,0 +1,7 @@
+# DAMO2 - Quincena 2
+
+
+## SpringTools
+
+Elaboracion del ejemplo de web sockets  y [VIDEO](https://youtu.be/YrsvjaGUG2o)
+
---
+++ Quincena2/spring-websocket/src/main/java/com/soymegh/websocket/Greeting.java
@@ -0,0 +1,28 @@
+package com.soymegh.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+
+    private String message;
+
+	public Greeting(String message) {
+		super();
+		this.setMessage(message);
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+    
+    
+}
